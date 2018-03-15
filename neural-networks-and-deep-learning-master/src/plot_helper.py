2784e245eb53ea06177da7a24c16fd5540e60674
Md Kauser Ahmmed <anik2137@gmail.com>
2018-03-15 12:27:26 -0500

    updated all

---
+++ neural-networks-and-deep-learning-master/src/plot_helper.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_training_accuracy(training_accuracy, num_epochs, 
+                           training_accuracy_xmin, training_set_size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(training_accuracy_xmin, num_epochs), 
+            [accuracy*100.0/training_set_size 
+             for accuracy in training_accuracy[training_accuracy_xmin:num_epochs]],
+            color='#2A6EA6')
+    ax.set_xlim([training_accuracy_xmin, num_epochs])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy (%) on the training data')
+    plt.show()
+
+def plot_training_cost(training_cost, num_epochs, training_cost_xmin):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(training_cost_xmin, num_epochs), 
+            training_cost[training_cost_xmin:num_epochs],
+            color='#2A6EA6')
+    
+    ax.set_xlim([training_cost_xmin, num_epochs])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the training data')
+    plt.show()
+    
+def plot_test_accuracy(test_accuracy, num_epochs, test_accuracy_xmin):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(test_accuracy_xmin, num_epochs), 
+            [accuracy/100.0 
+             for accuracy in test_accuracy[test_accuracy_xmin:num_epochs]],
+            color='#2A6EA6')
+    ax.set_xlim([test_accuracy_xmin, num_epochs])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy (%) on the test data')
+    plt.show()
+
+def plot_test_cost(test_cost, num_epochs, test_cost_xmin):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(test_cost_xmin, num_epochs), 
+            test_cost[test_cost_xmin:num_epochs],
+            color='#2A6EA6')
+    ax.set_xlim([test_cost_xmin, num_epochs])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the test data')
+    plt.show()
